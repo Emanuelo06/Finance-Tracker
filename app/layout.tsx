b0dcd8c3465aae63cf396b98f6cbba24dd3f8972
Emanuele Czofei <emanuelecz06@gmail.com>
2025-07-21 17:41:32 +0200

    removed unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 "use client";
-import type { Metadata } from "next";
+
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+
 import { AuthProvider } from "@/components/AuthProvider";
 
 
